6d720fdc8cc82f901fd584b5dd2359343d5cad89
Catalin Iosif <catalin.iosif.me@gmail.com>
2023-06-20 12:41:44 +0300

    Add docker and DO configuration

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,18 @@ use blog_newsletter::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("blog-newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to get configuration file!");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address).unwrap();
     let port = listener.local_addr().unwrap().port();
     println!("Running on port: {}", port);
