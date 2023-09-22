4b6082e6a5abaa11ac9a99073b16852cc9332cdd
Catalin Iosif <catalin.iosif.me@gmail.com>
2023-09-22 20:42:44 +0300

    config change for digital ocean vars

--- src/configuration.rs
+++ src/configuration.rs
@@ -50,28 +50,30 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let base_path = std::env::current_dir()
-        .expect("Failed to determine the current directory");
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
-// Detect the running environment.
-// Default to `local` if unspecified.
+
+    // Detect the running environment.
+    // Default to `local` if unspecified.
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT.");
     let environment_filename = format!("{}.yaml", environment.as_str());
     let settings = config::Config::builder()
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
+        ))
+        // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
         .add_source(
-            config::File::from(configuration_directory.join("base.yaml"))
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
         )
-        .add_source(
-            config::File::from(configuration_directory.join(environment_filename))
-        )
-        .add_source(config::Environment::with_prefix("APP")
-            .prefix_separator("_")
-            .separator("__")
-        )
-
         .build()?;
     settings.try_deserialize::<Settings>()
 }
