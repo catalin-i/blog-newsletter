8827b09cd3d22db3d28cf3d2c1bc3b076616aade
Catalin Iosif <catalin.iosif.me@gmail.com>
2024-01-30 19:56:37 +0200

    add subscriptions tokens table

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use blog_newsletter::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber("blog_newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let application = Application::build(configuration).await?;
